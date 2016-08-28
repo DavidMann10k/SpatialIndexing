4f9bc6b3454817ddd8975b32e56d96dc5bf9506e
DavidMann10k <davidmann10k@gmail.com>
2016-08-28 17:31:51 -0500

    cleanup comments

--- SpatialIndexing/Octree/DepthFirstPrintTreeOperation.cs
+++ SpatialIndexing/Octree/DepthFirstPrintTreeOperation.cs
@@ -7,10 +7,7 @@ namespace SpatialIndexing.Octree
         public void Execute(OctreeNode<T> node)
         {
             Console.WriteLine(String.Format("Node #{0} Nodes: {1} Values: {2}", node.Index, node.CountNodes(), node.CountValues()));
-            //foreach (T value in node.Values)
-            //{
-            //    Console.WriteLine(value.ToString());
-            //}
+
             foreach(OctreeNode<T> child in node.Children)
             {
                 child.PerformOperation(this);
